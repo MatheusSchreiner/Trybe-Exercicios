d28d55fe9757e63aeb370fdb4ad7260d6b6567f4
Matheus Schreiner <matheusrubisch@gmail.com>
2020-12-09 22:24:17 -0300

    Novos arquivos

---
+++ Introduction-WebDevelopment/Bloc_04/Day_03/Exercise_02.js
@@ -0,0 +1,9 @@
+let symbol = '*';
+let lineInput= '';
+let sizeTriangle = 5;
+
+
+for(var count = 0; count < sizeTriangle; count += 1){
+    lineInput += symbol;
+    console.log(lineInput);
+}
---
+++ Introduction-WebDevelopment/Bloc_04/Day_03/Exercise_03.js
@@ -0,0 +1,26 @@
+// let pyramidBase = 5;
+// let symbol = "*";
+// let lineInput = "";
+// let pyramidMiddle = (pyramidBase + 1) / 2;
+// let rightLine = pyramidMiddle;
+
+// for (var countLines = 0; countLines < pyramidMiddle; countLines +=1){
+//     for (var countColumns = 0; countColumns <= pyramidBase; countColumns += 1){
+//         if (countColumns  > rightLine){
+//             lineInput += symbol;
+//         } else {
+//             lineInput += " ";
+//         }
+//     }
+//     console.log(lineInput);
+//     lineInput = "";
+//     rightLine -= 1;
+// }
+
+let symbol = '*';
+let lineInput = '';
+
+
+for(var count = 0; count < lineInput; count += 1){
+    
+}
