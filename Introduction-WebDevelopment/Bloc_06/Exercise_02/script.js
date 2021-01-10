3b650d37e645a0870e593d5d86700321804519cb
Matheus Schreiner <matheusrubisch@gmail.com>
2021-01-10 00:27:13 -0300

    Exercicios Bloco 6

---
+++ Introduction-WebDevelopment/Bloc_06/Exercise_02/script.js
@@ -0,0 +1,94 @@
+const states = document.getElementById('state');
+function createStateOptions() {
+  const stateOptions = ['Selecione','Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+  let value = 1;
+  for (let index = 0; index < stateOptions.length; index += 1) {
+    const createOptions = document.createElement('option');
+    states.appendChild(createOptions).innerText = stateOptions[index];
+    states.appendChild(createOptions).value = stateOptions[index];
+  }
+}
+
+function validateData(data) {
+  if (data.indexOf('/') === 2 || data.indexOf('/') === 5) {
+    const day = data.substr(0, 2);
+    const month = data.substr(3, 2);
+    const year = data.substr(6, 4);
+    if ((day > 0 && day < 31) && (month > 0 && month <= 12) && (year > 0 && year.length === 4)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkData() {
+  const inputData = document.querySelector('.input-data');
+  let data = inputData.value;
+  const userData = validateData(data);
+  if (!userData && data.length) {
+    inputData.value = '';
+    alert('data invalida');
+    return false;
+  }
+  return userData;
+}
+
+function checkEmail() {
+  const email = document.querySelector('.email-input');
+  let insertedEmail = email.value;
+  const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(insertedEmail);
+  if (!emailFormat && insertedEmail.length) {
+    email.value = '';
+    alert('email inválido');
+    return false;
+  }
+  return emailFormat
+}
+
+function renderCurriculum(event) {
+  event.preventDefault();
+  const formElements = document.querySelectorAll('input');
+  for (let index = 0; index < formElements.length; index += 1) {
+    if (formElements[index].type === 'radio' && !formElements[index].checked) {
+      continue;
+    }
+    const userInput = formElements[index].value;
+    const dataUser = document.querySelector('.form-data');
+    if (checkEmail() && checkData()) {
+      const div = document.createElement('div');
+      div.className = 'div-curriculum';
+      div.innerHTML = userInput;
+      dataUser.appendChild(div);
+    }
+  }
+}
+
+const clearButton = document.querySelector('.clear-button');
+function clearFields() {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea')
+  const div = document.querySelectorAll('.div-curriculum');
+  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+    const userInput = formElements[index];
+    userInput.value = '';
+    textArea.value = '';
+    div[index].innerText = '';
+  }
+}
+
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', renderCurriculum);
+clearButton.addEventListener('click', clearFields);
+
+window.onload = function () {
+  createStateOptions();
+}
+
+let cpf = document.querySelector("#cpf");
+
+cpf.addEventListener("blur", function(){
+   if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
+});
+
+// document.querySelector('input[type="radio"]:checked');
+// com este codigó ele busca exatamente qual o item que está selecionado no input do tipo radio
---
+++ Introduction-WebDevelopment/Bloc_06/Exercise_04/README.md
@@ -0,0 +1,37 @@
+## Descrição das Atividades
+
+### Exercício 1
+
+Adicione uma propriedade que defina o tamanho Flexbox base para o container da imagem 268px e para o container do menu 500px 
+
+### Exercício 2
+
+Adicione uma propriedade Flexbox que modifique a ordem em que a logomarca e os itens do menu aparecem na tela. 
+
+### Exercício 3
+
+Adicione uma propriedade Flexbox que modifique o tamanho padrão do container do menu quando a largura da página for maior que 768px .
+
+### Exercício 4
+
+Utilize a propriedade align-self no elemento correto
+
+### Exercício 5
+
+Aplique uma base de 350px para seu container aside .
+
+### Exercício 6
+
+Aplique um alinhamento para centralizar e outro para expandir. 
+
+### Exercício 7
+
+Adicione uma propriedade que faça com que seu container de filmes tenha uma base de 700px e outra base de 16% para as div do container de seu card. 
+
+### Exercício 8
+
+Aplique uma propridade com o valor 10 que expanda o container de filmes.
+
+### Exercício 8
+
+Adicione uma propriedade com o valor 1 que faça com que suas div do container de filmes ocupe todo o espaço em branco. 
