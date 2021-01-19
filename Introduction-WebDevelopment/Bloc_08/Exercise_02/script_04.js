2f7ade3688ac5812dd641e97a4e36f0d3050a442
Matheus Schreiner <matheusrubisch@gmail.com>
2021-01-19 13:45:18 -0300

    Bloc 8 Exercise 2

---
+++ Introduction-WebDevelopment/Bloc_08/Exercise_02/script_04.js
@@ -0,0 +1,103 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
+]
+
+function oldBooks() {
+  // escreva seu código aqui
+
+  console.log(books.filter((elementFilter) => ((2020 - elementFilter.releaseYear) > 60)).sort((objA, objB) => (objA.releaseYear - objB.releaseYear)));
+
+  //ao invés de você utilizar o ano direto como 2020, você pode armazenar a data atual em uma variável pelo métidi Date(), olhe abaixo como ficaria a variável que armazenaria a data atual
+  const currentYear = new Date().getFullYear();
+  console.log(books.filter((elementFilter) => ((currentYear - elementFilter.releaseYear) > 60)).sort((objA, objB) => (objA.releaseYear - objB.releaseYear)));
+
+
+  return books.filter((elementFilter) => (elementFilter.releaseYear < currentYear - 60)).sort((objA, objB) => (objA.releaseYear - objB.releaseYear));
+}
+
+assert.deepStrictEqual(oldBooks(), expectedResult);
