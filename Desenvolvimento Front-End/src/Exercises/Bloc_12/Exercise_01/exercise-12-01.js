d890ea34c3870b4b7a0552d87a72f12809e9ec88
Matheus Schreiner <matheusrubisch@gmail.com>
2021-05-06 12:06:00 -0300

    bloco 11/12

--- Desenvolvimento Front-End/src/Exercises/Bloc_12/Exercise_01/exercise-12-01.js
+++ Desenvolvimento Front-End/src/Exercises/Bloc_12/Exercise_01/exercise-12-01.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import pokemons from '../../pokemons';
+// import pokemons from '../../pokemons';
 
 class Exercise1201 extends React.Component {
   render() {
     return (
       <div>
-        {console.log(pokemons)}
-        { pokemons.map((value, index) => <img key={ index } src={ value.image } /> ) }
       </div>
     );
   }
