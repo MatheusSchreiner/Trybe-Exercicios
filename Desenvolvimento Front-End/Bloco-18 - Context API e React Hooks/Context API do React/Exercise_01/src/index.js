788b8ebcb0539137375e0b89802a3021192c4985
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2021-06-22 14:28:32 -0300

    Bloco 18 - Context API do React "Atualização index.js"

--- Desenvolvimento Front-End/Bloco-18 - Context API e React Hooks/Context API do React/Exercise_01/src/index.js
+++ Desenvolvimento Front-End/Bloco-18 - Context API e React Hooks/Context API do React/Exercise_01/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Provider from './context/Provider';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-   <App />
-  </React.StrictMode>,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
