d10177477e0f83e1cf2d140902308f163bfa773f
Matheus Schreiner <matheusrubisch@gmail.com>
2021-09-18 00:26:40 -0300

    INSERT - Back - Bloco 28 - Bonus

---
+++ Desenvolvimento Back-End/Bloco 28 - Autenticacao e Upload de Arquivos/NodeJS JWT Json Web Token/Exercicios/Bonus/controllers/index.js
@@ -0,0 +1,13 @@
+const ping = require('./ping');
+const login = require('./login');
+const me = require('./me');
+const topsecret = require('./topsecret');
+const signup = require('./signup');
+
+module.exports = {
+  ping,
+  login,
+  me,
+  topsecret,
+  signup,
+};
